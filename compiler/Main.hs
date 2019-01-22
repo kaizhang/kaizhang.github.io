3b0469a2159fe9e888b7d7b98737dff293bc43e7
kaizhang <zk65900931@gmail.com>
2019-01-22 11:44:37 -0800

    update

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -4,7 +4,8 @@ import           Hakyll
 import Hakyll.Web.Sass (sassCompilerWith, sassDefConfig, SassOptions(..))
 
 saasOptions = sassDefConfig
-    { sassIncludePaths      = Just ["web/foundation/css"]
+    { sassIncludePaths      = Just [ "third_party/foundation-sites/scss"
+                                   , "third_party/motion-ui/src" ]
     }
 
 --------------------------------------------------------------------------------
@@ -16,17 +17,21 @@ main = hakyll $ do
         route $ gsubRoute "web/" (const "")
         compile copyFileCompiler
 
-    match "web/static/css/*" $ do
+    match "web/static/css/*.css" $ do
         route $ gsubRoute "web/" (const "")
         compile copyFileCompiler
 
-    match "web/static/js/*" $ do
+    match "web/static/css/app.scss" $ do
+        route $ composeRoutes (gsubRoute "web/" (const "")) $ setExtension "css"
+        compile $ sassCompilerWith saasOptions
+
+    match "web/static/js/*.js" $ do
         route $ gsubRoute "web/" (const "")
         compile copyFileCompiler
 
-    -- match "web/static/scss/app.scss" $ do
-    --    route $ constRoute "static/css/app.css"
-    --    compile $ sassCompilerWith saasOptions
+    match "third_party/foundation-sites/dist/js/foundation.min.js" $ do
+        route $ constRoute "static/js/foundation.min.js"
+        compile copyFileCompiler
 
     match "web/pages/*.html" $ do
         route $ gsubRoute "web/pages/" $ const ""
