d23bbabcfc34e14040a1a9907f528ea2b0010e85
Kai Zhang <kai@kzhang.org>
2021-11-24 18:52:20 -0800

    update

---
+++ compiler/Main.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid ((<>))
+import           Hakyll
+import Hakyll.Web.Sass (sassCompilerWith, sassDefConfig, SassOptions(..))
+
+import Lib.Compiler.CV (cvCompiler)
+
+saasOptions = sassDefConfig
+    { sassIncludePaths      = Just [ "third_party/foundation-sites/scss"
+                                   , "third_party/motion-ui/src" ]
+    }
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "web/templates/*" $ compile templateBodyCompiler
+
+    match "web/static/img/*" $ do
+        route $ gsubRoute "web/" (const "")
+        compile copyFileCompiler
+
+    match "web/static/css/*.css" $ do
+        route $ gsubRoute "web/" (const "")
+        compile copyFileCompiler
+
+    match "web/static/css/app.scss" $ do
+        route $ composeRoutes (gsubRoute "web/" (const "")) $ setExtension "css"
+        compile $ sassCompilerWith saasOptions
+
+    match "web/static/js/*.js" $ do
+        route $ gsubRoute "web/" (const "")
+        compile copyFileCompiler
+
+    match "third_party/foundation-sites/dist/js/foundation.min.js" $ do
+        route $ constRoute "static/js/foundation.min.js"
+        compile copyFileCompiler
+
+    match "web/pages/*.html" $ do
+        route $ gsubRoute "web/pages/" $ const ""
+        compile $ getResourceBody >>= loadAndApplyTemplate
+            "web/templates/default.html" defaultContext >>=
+            relativizeUrls
+
+    match "web/pages/cv.md" $ do
+        route $ constRoute "cv.html"
+        compile $ cvCompiler >>= loadAndApplyTemplate
+            "web/templates/cv.html" defaultContext >>=
+            relativizeUrls
+
